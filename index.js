d6f010383818a53b1892de3594f518dd2ff93f72
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2023-10-27 17:40:44 +0100

    Firebase DB created and setup/linked to index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://we-are-the-champions-f8748-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
