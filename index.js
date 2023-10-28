d922efe85f457947695a35663b39e33a10e76ed2
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2023-10-28 18:17:15 +0100

    Publish button event listener takes the value from the message, from and to fields and creates an object that is pushed to the DB when clicked

--- index.js
+++ index.js
@@ -10,14 +10,24 @@ const database = getDatabase(app);
 const endorsementsInDB = ref(database, "endorsements");
 
 const messageInputEl = document.getElementById("message-input");
+const fromInputEL = document.getElementById("from-input");
+const toInputEl = document.getElementById("to-input");
 const publishBtnEl = document.getElementById("publish-btn");
 const endorsementsListEl = document.getElementById("endorsements-list");
 
 publishBtnEl.addEventListener("click", function() {
-   const inputValue = messageInputEl.value;
+   const messageInputValue = messageInputEl.value;
+   const fromInputValue = fromInputEL.value;
+   const toInputValue = toInputEl.value;
    
-   if (inputValue !== "") {
-       push(endorsementsInDB, inputValue);
+   const totalInput = {
+       from: fromInputValue,
+       to: toInputValue,
+       message: messageInputValue
+   };
+   
+   if (messageInputValue !== "") {
+       push(endorsementsInDB, totalInput);
        
        clearMessageInputEl()
    }
